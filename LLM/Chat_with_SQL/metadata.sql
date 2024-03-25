c089db2c15efaa9349169a5bb53adac7c42149a7
Shriniwas Iyengar <Shrinix>
2024-03-25 10:58:43 -0400

    Used sqlcoder and new prompting style with metadata.sql

---
+++ LLM/Chat_with_SQL/metadata.sql
@@ -0,0 +1,151 @@
+REATE TABLE actor (
+	actor_id SERIAL NOT NULL,
+	first_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT actor_pkey PRIMARY KEY (actor_id)
+)
+CREATE TABLE address (
+	address_id SERIAL NOT NULL,
+	address VARCHAR(50) NOT NULL,
+	address2 VARCHAR(50),
+	district VARCHAR(20) NOT NULL,
+	city_id SMALLINT NOT NULL,
+	postal_code VARCHAR(10),
+	phone VARCHAR(20) NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT address_pkey PRIMARY KEY (address_id),
+	CONSTRAINT fk_address_city FOREIGN KEY(city_id) REFERENCES city (city_id)
+)
+CREATE TABLE category (
+	category_id SERIAL NOT NULL,
+	name VARCHAR(25) NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT category_pkey PRIMARY KEY (category_id)
+)
+CREATE TABLE city (
+	city_id SERIAL NOT NULL,
+	city VARCHAR(50) NOT NULL,
+	country_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT city_pkey PRIMARY KEY (city_id),
+	CONSTRAINT fk_city FOREIGN KEY(country_id) REFERENCES country (country_id)
+)
+CREATE TABLE country (
+	country_id SERIAL NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT country_pkey PRIMARY KEY (country_id)
+)
+CREATE TABLE customer (
+	customer_id SERIAL NOT NULL,
+	store_id SMALLINT NOT NULL,
+	first_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL,
+	email VARCHAR(50),
+	address_id SMALLINT NOT NULL,
+	activebool BOOLEAN DEFAULT true NOT NULL,
+	create_date DATE DEFAULT ('now'::text)::date NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
+	active INTEGER,
+	CONSTRAINT customer_pkey PRIMARY KEY (customer_id),
+	CONSTRAINT customer_address_id_fkey FOREIGN KEY(address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE film (
+	film_id SERIAL NOT NULL,
+	title VARCHAR(255) NOT NULL,
+	description TEXT,
+	release_year INTEGER,
+	language_id SMALLINT NOT NULL,
+	rental_duration SMALLINT DEFAULT 3 NOT NULL,
+	rental_rate NUMERIC(4, 2) DEFAULT 4.99 NOT NULL,
+	length SMALLINT,
+	replacement_cost NUMERIC(5, 2) DEFAULT 19.99 NOT NULL,
+	rating mpaa_rating DEFAULT 'G'::mpaa_rating,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	special_features TEXT[],
+	fulltext TSVECTOR NOT NULL,
+	CONSTRAINT film_pkey PRIMARY KEY (film_id),
+	CONSTRAINT film_language_id_fkey FOREIGN KEY(language_id) REFERENCES language (language_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE film_actor (
+	actor_id SMALLINT NOT NULL,
+	film_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT film_actor_pkey PRIMARY KEY (actor_id, film_id),
+	CONSTRAINT film_actor_actor_id_fkey FOREIGN KEY(actor_id) REFERENCES actor (actor_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT film_actor_film_id_fkey FOREIGN KEY(film_id) REFERENCES film (film_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE film_category (
+	film_id SMALLINT NOT NULL,
+	category_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT film_category_pkey PRIMARY KEY (film_id, category_id),
+	CONSTRAINT film_category_category_id_fkey FOREIGN KEY(category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT film_category_film_id_fkey FOREIGN KEY(film_id) REFERENCES film (film_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE inventory (
+	inventory_id SERIAL NOT NULL,
+	film_id SMALLINT NOT NULL,
+	store_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT inventory_pkey PRIMARY KEY (inventory_id),
+	CONSTRAINT inventory_film_id_fkey FOREIGN KEY(film_id) REFERENCES film (film_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE language (
+	language_id SERIAL NOT NULL,
+	name CHAR(20) NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT language_pkey PRIMARY KEY (language_id)
+)
+CREATE TABLE payment (
+	payment_id SERIAL NOT NULL,
+	customer_id SMALLINT NOT NULL,
+	staff_id SMALLINT NOT NULL,
+	rental_id INTEGER NOT NULL,
+	amount NUMERIC(5, 2) NOT NULL,
+	payment_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	CONSTRAINT payment_pkey PRIMARY KEY (payment_id),
+	CONSTRAINT payment_customer_id_fkey FOREIGN KEY(customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT payment_rental_id_fkey FOREIGN KEY(rental_id) REFERENCES rental (rental_id) ON DELETE SET NULL ON UPDATE CASCADE,
+	CONSTRAINT payment_staff_id_fkey FOREIGN KEY(staff_id) REFERENCES staff (staff_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE rental (
+	rental_id SERIAL NOT NULL,
+	rental_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	inventory_id INTEGER NOT NULL,
+	customer_id SMALLINT NOT NULL,
+	return_date TIMESTAMP WITHOUT TIME ZONE,
+	staff_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT rental_pkey PRIMARY KEY (rental_id),
+	CONSTRAINT rental_customer_id_fkey FOREIGN KEY(customer_id) REFERENCES customer (customer_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT rental_inventory_id_fkey FOREIGN KEY(inventory_id) REFERENCES inventory (inventory_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT rental_staff_id_key FOREIGN KEY(staff_id) REFERENCES staff (staff_id)
+)
+CREATE TABLE staff (
+	staff_id SERIAL NOT NULL,
+	first_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL,
+	address_id SMALLINT NOT NULL,
+	email VARCHAR(50),
+	store_id SMALLINT NOT NULL,
+	active BOOLEAN DEFAULT true NOT NULL,
+	username VARCHAR(16) NOT NULL,
+	password VARCHAR(40),
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	picture BYTEA,
+	CONSTRAINT staff_pkey PRIMARY KEY (staff_id),
+	CONSTRAINT staff_address_id_fkey FOREIGN KEY(address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+CREATE TABLE store (
+	store_id SERIAL NOT NULL,
+	manager_staff_id SMALLINT NOT NULL,
+	address_id SMALLINT NOT NULL,
+	last_update TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+	CONSTRAINT store_pkey PRIMARY KEY (store_id),
+	CONSTRAINT store_address_id_fkey FOREIGN KEY(address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT store_manager_staff_id_fkey FOREIGN KEY(manager_staff_id) REFERENCES staff (staff_id) ON DELETE RESTRICT ON UPDATE CASCADE
+)
+
+-- city.country_id can be joined with country.country_id
--- LLM/Chat_with_SQL/sql_app-psql-updated.py
+++ LLM/Chat_with_SQL/sql_app-psql-updated.py
@@ -2,7 +2,7 @@ from langchain_community.llms import CTransformers
 from langchain.sql_database import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain.chains import create_sql_query_chain
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.agents import create_sql_agent
 from langchain.agents.agent_toolkits import SQLDatabaseToolkit
@@ -11,9 +11,56 @@ from langchain.agents.agent_types import AgentType
 from langchain_community.chat_models import ChatOllama
 import re
 from db_connectors import PostgresConnector
+import psycopg2
+from tabulate import tabulate
 
-if __name__ == '__main__':
+def generate_prompt(question, prompt_file="prompt.md", metadata_file="metadata.sql"):
+    with open(prompt_file, "r") as f:
+        prompt = f.read()
 
+    with open(metadata_file, "r") as f:
+        table_metadata_string = f.read()
+
+    prompt = prompt.format(
+        user_question=question, table_metadata_string=table_metadata_string
+    )
+
+    prompt = PromptTemplate.from_template(
+        prompt, metadata={"user_question": question, "table_metadata_string": table_metadata_string}
+    )
+    return prompt
+
+def query_model(question, llm):
+    prompt = generate_prompt(question)
+
+    output_parser = StrOutputParser()
+
+    chain = prompt | llm | output_parser
+
+    # output = llm(
+    #     prompt,
+    #     max_tokens=200, # Generate up to 32 tokens
+    #     stop=["Q:", "\n"],
+    #     echo=True # Echo the prompt back in the output
+    # )
+
+    response = chain.invoke(
+        {"input":prompt}
+    )
+
+   # sql_query = response['choices'][0]['text'].split('[SQL]')[1]
+    print("SQL: ", response)
+
+    return response
+    #return execute_query(response)
+
+def execute_query(query, cur):
+    cur.execute(query)
+    rows = cur.fetchall()
+    headers = [desc[0] for desc in cur.description]
+    return tabulate(rows, headers=headers, tablefmt="grid")
+
+def main():
 
     # llm = CTransformers(model="//Users//shriniwasiyengar//.cache//lm-studio//models//TheBloke//nsql-llama-2-7B-GGUF",
     #                     model_type="llama",
@@ -21,12 +68,13 @@ if __name__ == '__main__':
     #                             'temperature': 0.01,
     #                             'context_length': 6000})
 
-
-
     # Defining the models to be used
-    #llm = ChatOllama(model='sqlcoder')
-    llm = ChatOllama(model='llama2', config={'max_new_tokens': 512, 'temperature': 0.01, 'context_length': 6000})
+    LLM = ChatOllama(model='sqlcoder')
+    #llm = ChatOllama(model='sqlcoder', config={'max_new_tokens': 512, 'temperature': 0.01, 'context_length': 6000})
 
+    from huggingface_hub import snapshot_download
+    # model_path = snapshot_download(repo_id="defog/sqlcoder-7b-2", repo_type="model",
+    #                                local_dir="../models/sqlcoder-7b-2", local_dir_use_symlinks=False)
     host = 'localhost'
     port = '5432'
     username = 'postgres'
@@ -36,106 +84,45 @@ if __name__ == '__main__':
     pg_uri = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{mydatabase}"
 
     db = SQLDatabase.from_uri(pg_uri,
-                              include_tables=['actor', 'address', 'category', 'city', 'country', 'customer', 'film', 'film_actor', 'film_category', 'inventory', 'language', 'payment', 'rental', 'staff', 'store'],
+                              include_tables=['actor', 'address', 'category', 'city', 'country', 'customer', 'film',
+                                              'film_actor', 'film_category', 'inventory', 'language', 'payment',
+                                              'rental', 'staff', 'store'],
                               sample_rows_in_table_info=2)
     table_info = db.get_table_info()
     #print(db.table_info)
     dialect = db.dialect
+    print("Welcome to the SQLCoder Chat Interface!")
+    print("Type your message and press Enter to get a response.")
+    print("Type 'exit' to quit.")
 
-    #write a loop to get all the columns in each of the tables in table_info
-    # for table in table_info:
-    #     print(PostgresConnector(pg_uri).get_keys(table))
+    # conn = psycopg2.connect(
+    #     dbname="yelp",
+    #     user="postgres",
+    #     password="postgres",
+    #     host="localhost",
+    #     port="5432"
+    # )
+    # cur = conn.cursor()
+    #
 
-    system = (
-        f"You are a {dialect} expert. Given an input question, create a syntactically correct {dialect} query to run."
-        "Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per {dialect}. You can order the results to return the most informative data in the database."
-        "Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (\") to denote them as delimited identifiers."
-        "Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table."
-        "Pay attention to use date('now') function to get the current date, if the question involves \"today\"."
+    # from llama_cpp import Llama
+    import psycopg2
+    from tabulate import tabulate
 
-        "Only use the following tables and columns based on the schema below:"
-        f"{table_info}"
+    #LLM = Llama(model_path="/Users/shriniwasiyengar/Downloads/sqlcoder-7b-q5_k_m.gguf", n_gpu_layers=10, n_ctx=2048,
+     #           verbose=False)
 
-        f"Write an initial draft of the query. Then double check the {dialect} query for common mistakes, including:"
-        "- Using NOT IN with NULL values"
-        "- Using UNION when UNION ALL should have been used"
-        "- Using BETWEEN for exclusive ranges"
-        "- Data type mismatch in predicates"
-        "- NOT Properly quoting identifiers"
-        "- NOT Using the correct number of arguments for functions"
-        "- NOT Casting to the correct data type"
-        "- NOT Using the proper columns for joins"
-        "- NOT Using the correct table aliases"
-        "- NOT Using the correct table names as defined in the schema"
-        "- NOT Using the correct column names as defined in the schema"
-        "- NOT Using correct column names in GROUP BY and ORDER BY clauses"
-        "- NOT Keeping the queries simple and easy to understand"
-        "- NOT Using the correct syntax for the {dialect} query"
-        "- Using unnecessary subqueries and conditions"
+    while True:
+        user_input = input("Question: ")
+        if user_input.lower() == 'exit':
+            print("Exiting...")
+            # cur.close()
+            # conn.close()
+            break
+        print("Answer: ")
+        response = query_model(user_input, LLM)
+        print(response)
 
-        "Use format:"
-        "Final answer: <<FINAL_ANSWER_QUERY>>")
-
-    prompt = ChatPromptTemplate.from_messages(
-        [("system", system), ("human", "{input}")])
-
-    #prompt = prompt.input_variables(dialect=db.dialect, table_info=table_info)
-    prompt.input_variables.append('table_info')
-    # print(prompt.input_variables[0])
-    # print(type(prompt.input_variables))
-
-    question = "How many films have film length was more than 60 minutes?"
-    #question = "Find the total number of actors"
-    #question = "Movies in how many different languages are available in the store?"
-    #question = "What is the total number of films in the store?"
-
-    # input = question
-
-    def parse_final_answer(text):
-        print(text)
-        pattern = r"\n```(.*?)\n```\n"
-        match = re.search(pattern, text, re.DOTALL)
-
-        if match:
-            result = match.group(1)
-            print(result)
-        return result
-
-    chain = create_sql_query_chain(llm, db, prompt=prompt)
-    #prompt.pretty_print()
-
-    answer = chain.invoke(
-        {
-            "question": question
-        }
-    )
-    print(answer)
-    print("-----------")
-    print("-----------")
-    print("-----------")
-    system2 = (
-        f"Cross-reference the {answer} against the schema below."
-        f"{table_info}"
-
-        f"Double check the query for the following:"
-        f" -Correct SQL syntax as per {dialect}"
-         "- Ensure that the table names in the query are in the list ['actor', 'address', 'category', 'city', 'country', 'customer', 'film', 'film_actor', 'film_category', 'inventory', 'language', 'payment', 'rental', 'staff', 'store']"         
-        
-        "Only return the final corrected SQL query")
-
-    prompt2 = ChatPromptTemplate.from_messages(
-        [("system", system2), ("human", "{input}")])
-
-    # prompt = prompt.input_variables(dialect=db.dialect, table_info=table_info)
-    prompt2.input_variables.append('table_info')
-    prompt2.input_variables.append('top_k')
-    query1 = parse_final_answer(answer)
-
-    chain2 = create_sql_query_chain(llm, db, prompt=prompt2)
-    query2 = chain2.invoke(
-        {
-            "question": query1
-        }
-    )
-    print(query2)
+if __name__ == "__main__":
+    main()
 
